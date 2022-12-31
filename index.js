c57335c2656f590d273d97ac71ed2abb1266d10b
root <bobwatcherx@gmail.com>
2022-12-31 12:05:03 +0700

    example

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express(); // notice that the app instance is called `app`, this is very important.
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// no need for `app.listen()` on Deta, we run the app automatically.
+module.exports = app; // make sure to export your `app` instance.
